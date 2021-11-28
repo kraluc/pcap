db8e7d0f1cda109cd05ea8a47c4a51f22e7a3dd7
Vincent Ricci <riccivm@gmail.com>
2021-11-28 11:56:55 -0800

    handled negative number and numbers with leading '+'

---
+++ progs/math_validation.py
@@ -0,0 +1,43 @@
+import math
+
+
+def valid_float(text: str) -> float:
+    # remove spaces
+    text = text.strip()
+
+    # Check whether text is a positive number
+    if text == "":
+        raise ValueError
+    elif text.find("+") == 0 and text.rfind("+") == 0:  # leading +
+        if len(text) < 2:
+            raise ValueError
+        elif text[1::].isdecimal():
+            return float(text)
+        elif text[1::].find(".") == text[1::].rfind("."):
+            return float(text)
+        else:
+            raise ValueError
+    elif text.isdecimal():
+        return float(text)
+    elif text.find(".") == text.rfind("."):
+        if len(text) < 2:
+            raise ValueError
+        elif text.replace(".", "").isdecimal():  # No leading + with ONE '.' separator"
+            return float(text)
+        else:
+            raise ValueError
+    else:
+        raise ValueError
+
+
+# get a valid float (this test ensures the number is positive but misses the case where someone enters +1.3)
+while True:
+    try:
+        x = valid_float(input("Enter a positive float: "))
+        break
+    except ValueError:
+        print("Try again")
+
+y = math.sqrt(x)
+
+print("The square root of", x, "equals to", y)
